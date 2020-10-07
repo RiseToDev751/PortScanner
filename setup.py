67a558ac93cc2fd47c4d8f12eb549cba53bddc1c
xRiseStrqfe <68116540+xRiseStrqfe@users.noreply.github.com>
2020-10-07 21:09:17 +0300

    A Littel Wrong

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from colorama import Fore, Back, Style
 
 user = os.system("$USER")
 
-if user != root:
-    os.system("sudo python3 setup.py")
+if user != "root":
+    print("Lütfen Komudun Başına Sudo Koyunuz")
+    os.system("apt install nmap")
+    os.system("pip3 install -r requirements.txt")
+    os.system("python3 PortScanner.py")
 else:
     os.system("apt install nmap")
     os.system("pip3 install -r requirements.txt")
