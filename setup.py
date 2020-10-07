aaab671966d793a2ec2871aca51d510480a88705
xRiseStrqfe <68116540+xRiseStrqfe@users.noreply.github.com>
2020-10-07 21:11:35 +0300

    Sorry

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from colorama import Fore, Back, Style
 user = os.system("$USER")
 
 if user != "root":
-    print("Lütfen Komudun Başına Sudo Koyunuz")
+    print(Fore.RED,"Lütfen Komudun Başına Sudo Koyunuz")
     os.system("apt install nmap")
     os.system("pip3 install -r requirements.txt")
     os.system("python3 PortScanner.py")
